66b506b855761d8aa3878b6ec1087cc76c0511b1
dsr <d.singhranger@yahoo.co.uk>
2022-12-19 23:13:58 +0000

    adding pseudocode to script_psc file for challenge

---
+++ script_psc.js
@@ -0,0 +1,35 @@
+// this challenge is about achieving the following:
+
+// total number of months included in the dataset
+
+// the net total amount of Profit/Losses over the entire period
+
+// average of the **changes** in Profit/Losses over the entire period. You will need to track what the total change in profits are from month to month and then find the average.
+//   * (`Total/Number of months`)
+
+// The greatest increase in profits (date and amount) over the entire period.
+
+// the greatest decrease in losses (date and amount) over the entire period.
+
+// When you open your code in the browser your resulting analysis should look similar to the following:
+
+//   ```text
+//   Financial Analysis
+//   ----------------------------
+//   Total Months: 25
+//   Total: $2561231
+//   Average  Change: $-2315.12
+//   Greatest Increase in Profits: Feb-2012 ($1926159)
+//   Greatest Decrease in Profits: Sep-2013 ($-2196167)
+//   ```
+
+// Your final code should print the analysis to the console.
+
+// determine the toal number of months in dataset = to use an array
+// the array could be month = [dataset = string]
+// second array would be finances per month: monthlyFinances = [numbers]
+// array position in month would need to correspond with that in monthlyFinances:
+
+// month array[0] would correspond to monthlyFinances array[0].
+// Need to find out how to link arrays - concatenate function NumPy function
+// https://www.w3schools.com/python/numpy/numpy_array_join.asp
