03cf4846c4990588a3e643f1eeca7f60d8448fab
dsr <d.singhranger@yahoo.co.uk>
2022-12-20 20:35:38 +0000

    pseudocode about creating best type of array for the task

--- script_psc.js
+++ script_psc.js
@@ -26,10 +26,29 @@
 // Your final code should print the analysis to the console.
 
 // determine the toal number of months in dataset = to use an array
-// the array could be month = [dataset = string]
-// second array would be finances per month: monthlyFinances = [numbers]
-// array position in month would need to correspond with that in monthlyFinances:
 
-// month array[0] would correspond to monthlyFinances array[0].
+// I will need to create 2 arrays to determine the total number of months in dataset
+// and financial aspects - total amount of profit/losses over entire period, average change
+
+
+// The Array
+
+// With both arrays I can then  work out the increase and decrease of profits and losses (date and amount)
+
+
+// the array is multi-dimensional so will need to create a multi-dimensional array:
+// https://www.w3schools.com/java/java_arrays_multi.asp
+
+// or if this doesn't work create two arrays
+
+
 // Need to find out how to link arrays - concatenate function NumPy function
 // https://www.w3schools.com/python/numpy/numpy_array_join.asp
+
+// total months = total items in months array = .length.
+// total amount of losses/profits = maths + function in array need to use + function for an array
+// link: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Addition or
+// link: https://www.javatpoint.com/program-to-print-the-sum-of-all-the-elements-of-an-array
+
+// in order to display it options = console.log, alert or write  some lines in html to link with 
+// js and perhaps do some CSS coding
